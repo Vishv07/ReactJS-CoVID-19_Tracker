6e830845ee230cb61faf985d4da6d95f522edb3a
Vishv <krishnakakadiya9@gmai.com>
2020-05-23 18:34:02 +0530

    Comi

---
+++ build/precache-manifest.83adab77eb0c9806714192df6e21a4ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1fd4b51155b7a9da22",
+    "url": "/ReactJS-COVID19-Tracker/static/js/runtime~main.dbaeb1b4.js"
+  },
+  {
+    "revision": "68913099827ad3d01435",
+    "url": "/ReactJS-COVID19-Tracker/static/js/main.3a8b352c.chunk.js"
+  },
+  {
+    "revision": "1e84e33b19e15aef6f8c",
+    "url": "/ReactJS-COVID19-Tracker/static/js/2.35b82d30.chunk.js"
+  },
+  {
+    "revision": "68913099827ad3d01435",
+    "url": "/ReactJS-COVID19-Tracker/static/css/main.b89d0b6e.chunk.css"
+  },
+  {
+    "revision": "c0d2d88c0a2eac47480c5661985d07b4",
+    "url": "/ReactJS-COVID19-Tracker/index.html"
+  }
+];
